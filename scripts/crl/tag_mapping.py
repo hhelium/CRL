0d8958182f430b89241b64b78460b6f46b4735af
= <dangtm24@gmail.com>
2020-12-21 21:27:56 -0600

    CRL's POS tags

---
+++ scripts/crl/tag_mapping.py
@@ -0,0 +1,39 @@
+"""
+# @ Our valid POS tags:
+    "NN", "NNS",
+    "NNP", "NNPS",
+    "PRP",
+    "POS_of", "POS",
+    "CD", "MN",
+    "DET",
+    "VBZ", "VB", "VBN", "VBG", "MD",
+    "JJ", "JJR", "JJS", "MOST", "MORE", "AND", "OR",
+    "THAN", "AS",
+    "RB", "RBR", "RBS",
+    "BY", "TO",
+    "IN",
+    "AUX", "AUX_DO",
+    "WHO", "WHERE", "WHICH", "HOW",
+    "WP", "WDT",
+    "THERE",
+    "PERIOD", "QUESTION", "EXCLAM",
+
+    "RP",
+    "PREP",
+    "VAR",
+    "PLEASE"
+"""
+
+spacy_tag2our_tag = {
+    "DT": "DET",
+    ".": "PERIOD",
+    "-LRB-": "LRB",
+    "-RRB-": "RRB",
+    ",": "COMMA",
+    "`": "QUOTE",
+    ";": "SEMI_COLON",
+    ":": "COLON",
+    "PRP$": "POS_pronoun"
+}
+
+other_tag2our_tag = spacy_tag2our_tag
